ac20d3755861d0a74f02309282e84ca05f526438
michaeldini <michael.dini@gmail.com>
2022-11-18 12:21:14 -0500

    second comit

--- secondproject/_modidx.py
+++ secondproject/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://michaeldini.github.io',
                 'git_url': 'https://github.com/michaeldini/secondproject',
                 'lib_path': 'secondproject'},
-  'syms': {'secondproject.core': {'secondproject.core.foo': ('core.html#foo', 'secondproject/core.py')}}}
+  'syms': { 'secondproject.core': {'secondproject.core.foo': ('core.html#foo', 'secondproject/core.py')},
+            'secondproject.secondproject': {'secondproject.secondproject.foo': ('core.html#foo', 'secondproject/secondproject.py')}}}
